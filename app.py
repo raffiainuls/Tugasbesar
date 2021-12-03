3999a8f9426b52b7315d0c52b3db23e6d45471d2
raffiainuls <92317626+raffiainuls@users.noreply.github.com>
2021-12-03 19:03:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_word():
-    return render_template("index.html")
 
-if __name__=="__main__":
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+
+if __name__ == "__main__":
     app.run(debug=True)
+    
