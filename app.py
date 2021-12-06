fe1202fba192a27790174e6956d0405d2505257d
raffiainuls <92317626+raffiainuls@users.noreply.github.com>
2021-12-06 09:32:47 +0700

    deploy

--- app.py
+++ app.py
@@ -1,126 +1,57 @@
-import os
-import uuid
-from flask import Flask
-import urllib
+import base64
+import numpy as np
+import io
 from PIL import Image
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.models import load_model
-from flask import Flask , render_template  , request , send_file
-from tensorflow.keras.preprocessing.image import load_img , img_to_array
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import img_to_array
+from flask import request
+from flask import jsonify
+from flask import Flask
 
 app = Flask(__name__)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model = load_model(os.path.join(BASE_DIR , 'model.hdf5'))
+def get_model():
+    global model
+    model = load_model("model.h5")
+    print("*model is loaded")
 
 
-ALLOWED_EXT = set(['jpg' , 'jpeg' , 'png' , 'jfif'])
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+def preprocess_image(image, target_size):
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    image = image.resize(target_size)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
 
-classes = ['airplane' ,'automobile', 'bird' , 'cat' , 'deer' ,'dog' ,'frog', 'horse' ,'ship' ,'truck']
-
-
-def predict(filename , model):
-    img = load_img(filename , target_size = (32 , 32))
-    img = img_to_array(img)
-    img = img.reshape(1 , 32 ,32 ,3)
-
-    img = img.astype('float32')
-    img = img/255.0
-    result = model.predict(img)
-
-    dict_result = {}
-    for i in range(10):
-        dict_result[result[0][i]] = classes[i]
-
-    res = result[0]
-    res.sort()
-    res = res[::-1]
-    prob = res[:3]
-    
-    prob_result = []
-    class_result = []
-    for i in range(3):
-        prob_result.append((prob[i]*100).round(2))
-        class_result.append(dict_result[prob[i]])
-
-    return class_result , prob_result
+    return image
 
 
+print(" *Loading Keras model....")
+get_model()
+@app.route("/predict", methods=["POST"])
+def predict():
+    message = request.get_json(force=True)
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocess_image(image, target_size=(32, 32))
 
+    prediction = model.predict(processed_image).tolist()
 
-@app.route('/')
-def home():
-        return render_template("index.html")
-
-@app.route('/success' , methods = ['GET' , 'POST'])
-def success():
-    error = ''
-    target_img = os.path.join(os.getcwd() , 'static/images')
-    if request.method == 'POST':
-        if(request.form):
-            link = request.form.get('link')
-            try :
-                resource = urllib.request.urlopen(link)
-                unique_filename = str(uuid.uuid4())
-                filename = unique_filename+".jpg"
-                img_path = os.path.join(target_img , filename)
-                output = open(img_path , "wb")
-                output.write(resource.read())
-                output.close()
-                img = filename
-
-                class_result , prob_result = predict(img_path , model)
-
-                predictions = {
-                      "class1":class_result[0],
-                        "class2":class_result[1],
-                        "class3":class_result[2],
-                        "prob1": prob_result[0],
-                        "prob2": prob_result[1],
-                        "prob3": prob_result[2],
-                }
-
-            except Exception as e : 
-                print(str(e))
-                error = 'This image from this site is not accesible or inappropriate input'
-
-            if(len(error) == 0):
-                return  render_template('success.html' , img  = img , predictions = predictions)
-            else:
-                return render_template('index.html' , error = error) 
-
-            
-        elif (request.files):
-            file = request.files['file']
-            if file and allowed_file(file.filename):
-                file.save(os.path.join(target_img , file.filename))
-                img_path = os.path.join(target_img , file.filename)
-                img = file.filename
-
-                class_result , prob_result = predict(img_path , model)
-
-                predictions = {
-                      "class1":class_result[0],
-                        "class2":class_result[1],
-                        "class3":class_result[2],
-                        "prob1": prob_result[0],
-                        "prob2": prob_result[1],
-                        "prob3": prob_result[2],
-                }
-
-            else:
-                error = "Please upload images of jpg , jpeg and png extension only"
-
-            if(len(error) == 0):
-                return  render_template('success.html' , img  = img , predictions = predictions)
-            else:
-                return render_template('index.html' , error = error)
-
-    else:
-        return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug = True)
-
-
+    response = {
+        'prediction': {
+            'airplane': prediction[0][0],
+            'automobile': prediction[0][1],
+            'bird': prediction[0][2],
+            'cat': prediction[0][3],
+            'deer': prediction[0][4],
+            'dog': prediction[0][5],
+            'frog': prediction[0][6],
+            'horse': prediction[0][7],
+            'ship': prediction[0][8],
+            'truck': prediction[0][9]
+        }
+    }
+    return jsonify(response)
